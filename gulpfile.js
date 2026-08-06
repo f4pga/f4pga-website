b10b7235f5c343ab16d4f5dfabf58d01f2d0a199
Karol Gugala <kgugala@antmicro.com>
2026-08-06 18:44:15 +0200

    Update dependencies
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp'),
-  sass = require('gulp-sass'),
+  sass = require('gulp-sass')(require('sass')),
   prefix = require('gulp-autoprefixer'),
   babel = require('gulp-babel'),
   uglify = require('gulp-uglify'),
